85bb6025e53394c31bf2a0df5281d1da8a850de9
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-10-11 19:54:08 +0530

    Functional Components

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const element=ReactDOM.createRoot(document.getElementById("element"));
-const head1=React.createElement("h4",{id:"head1",key:"h1"},"Heading 1");
-console.log(head1);
-const head2=React.createElement("h4",{id:"head2",key:"h2"},"Heading 2");
-const container=React.createElement("div",{id:"container",hello:"hfudbfu"},[head1,head2]);
-element.render(container);
+
+const Head1=()=>{
+    return(
+        
+        <div>
+            {heading2}
+            <h1>This is Heading</h1>
+            <h2>Heading 2</h2>
+        </div>
+    );
+};
+
+const heading2=(
+    <h1 id="title" key="h2">
+        Namasthe React
+    </h1>
+)
+
+element.render(<Head1/>);
