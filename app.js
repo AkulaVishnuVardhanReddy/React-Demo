ba28dd329c9e5eb6058b81403edf596cb97ba36b
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-10-12 07:26:47 +0530

    Food Villa Header

--- app.js
+++ app.js
@@ -3,21 +3,44 @@ import ReactDOM from 'react-dom/client'
 
 const element=ReactDOM.createRoot(document.getElementById("element"));
 
-const Head1=()=>{
+const Title=()=>{
     return(
-        
-        <div>
-            {heading2}
-            <h1>This is Heading</h1>
-            <h2>Heading 2</h2>
+        <div className='header-left'>
+            <img className='img-logo'
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9AIyWJnv-qeoSVFHoDSoWPoYpqTm_vWoxcg&s"/>
+            <h3>Food Villa</h3>
+        </div>
+    )
+}
+
+const LinksHeader=()=>{
+    return(
+        <ul className='links-top'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Carts</li>
+        </ul>
+    )
+}
+
+const Header=()=>{
+    return(
+        <div className='header-container'>
+            <Title />
+            <LinksHeader />
         </div>
     );
 };
 
-const heading2=(
-    <h1 id="title" key="h2">
-        Namasthe React
-    </h1>
-)
+const AppLayout=()=>{
+    return(
+        <div>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
+}
 
-element.render(<Head1/>);
+element.render(<Header/>);
