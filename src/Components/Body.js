82b9bbe66217d422d24c47f268e54b14a430e452
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-10-16 18:48:38 +0530

    Get Data from API

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,46 +1,68 @@
 import { useEffect, useState } from "react";
-import { CDN_IMG_LINK,RestaurentList } from "../Config";
+import { CDN_IMG_LINK } from "../Config";
+import Shimmer from "../Shimmer";
+
 
 function FilterBySearch(searchInput,restaurant){
-    console.log(searchInput);
-    return RestaurentList.filter((restaurant)=>restaurant.card.card.info.name.includes(searchInput));
+    return restaurant.filter((restaurant)=>restaurant.card.card.info.name.toLowerCase().includes(searchInput.toLowerCase()));
 }
 
 
-
-
 const Body=()=>{
-    const[restaurant,setRestaurent]=useState(RestaurentList);
+
+
+    const[restaurant,setRestaurent]=useState([]);
+    const[allrestaurant,setAllRestaurent]=useState([]);
     const [searchInput,setSearchInput]=useState("");
+    const[found,setFound]=useState("yes");
+
+
     useEffect(() => {
         getRestaurants();
     }, []);
     
     async function getRestaurants() {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.6287557&lng=79.4191795&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.6287557&lng=79.4191795&collection=80463&tags=layout_BAU_Contextual%2Cnoodles&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
         const json = await data.json();
-        console.log(json);
+        json?.data?.cards.splice(0,3);
+        setAllRestaurent(json?.data?.cards);
+        setRestaurent(json?.data?.cards);
     }
-    return(
+
+    return allrestaurant.length===0? <Shimmer/>: (
         <>
         <div className="search-container">
+
+
             <input type="text" 
                 placeholder="Search" 
                 value={searchInput}
                 onChange={(e)=>setSearchInput(e.target.value)}
                 className="input-serch"/>
+
+
             <button type="button" className="search-button" 
             onClick={()=>{
-                const data=FilterBySearch(searchInput,restaurant)
-                setRestaurent(data)
+                const data=FilterBySearch(searchInput,allrestaurant)
+                setRestaurent(data);
+                if(data.length===0){
+                    console.log(data);
+                    setFound("no")
+                }
+                else{
+                    setFound("yes")
+                }
                 }}>
                 Search
             </button>
         </div>
+
+        {found==="no" && <h1>Not Found..</h1>}
+        
         <div className='restaurent-cards'>
         {
             restaurant.map(restaurant=>{
-                return(<RestaurentCard{...restaurant.card.card.info} />);
+                return(<RestaurentCard{...restaurant?.card?.card?.info} key={restaurant?.card?.card?.info?.id}/>);
             })
         }
         </div>
@@ -51,10 +73,10 @@ const Body=()=>{
 const RestaurentCard=(props)=>{
     return(
         <div className='card'>
-            <img src={CDN_IMG_LINK+props.cloudinaryImageId}/>
-            <h2>{props.name}</h2>
-            <h3>{props.cuisines.join(" , ")}</h3>
-            <h4>{props.avgRating} stars</h4>
+            <img src={CDN_IMG_LINK+props?.cloudinaryImageId}/>
+            <h2>{props?.name}</h2>
+            <h3>{props.locality}</h3>
+            <h4>{props?.avgRating} stars</h4>
         </div>
     )
 }
